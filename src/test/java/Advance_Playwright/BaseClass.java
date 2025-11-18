3beaf4e10e02fd3a71b84cb51aeeacfc9b3a3997
S M Nahid Iqbal <nahid152553@gmail.com>
2025-11-19 01:07:09 +0600

    Updated locator

--- src/test/java/Advance_Playwright/BaseClass.java
+++ src/test/java/Advance_Playwright/BaseClass.java
@@ -1,9 +1,6 @@
 package Advance_Playwright;
 
-import com.microsoft.playwright.Browser;
-import com.microsoft.playwright.BrowserContext;
-import com.microsoft.playwright.Page;
-import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.*;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -19,7 +16,8 @@ public class BaseClass {
     @BeforeAll
     static void launchBrowser(){
         playwright = Playwright.create();
-        browser = playwright.chromium().launch();
+      //  browser = playwright.chromium().launch();
+        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false).setSlowMo(1000));
     }
     @AfterAll
     static void closeBrowser(){
--- src/test/java/Advance_Playwright/FormSubmission.java
+++ src/test/java/Advance_Playwright/FormSubmission.java
@@ -1,6 +1,36 @@
 package Advance_Playwright;
 
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.Locator;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.options.AriaRole;
+import org.junit.jupiter.api.Test;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+
 public class FormSubmission extends BaseClass{
-//    BaseClass baseClass = new BaseClass();
+
+    @Test
+    void clickSignUpForm(){
+        page.navigate("https://qavbox.github.io/demo/");
+        assertThat(page.locator("a[href='/demo/signup']")).isVisible();
+        page.locator("a[href='/demo/signup']").click();
+//        assertThat(page).hasURL("https://qavbox.github.io/demo/signup/");
+        String Name = page.getByText("Full N").textContent();
+        System.out.println(Name);
+        page.locator("//input[@id='username']").fill("S M Nahid Iqbal");
+
+        String gender = page.getByText("Gender:").textContent();
+        System.out.println(gender);
+        page.locator("//select[@name='sgender']").click();
+        page.locator("select[name='sgender']").selectOption("male");
+        page.locator("select[name='sgender']").selectOption("female");
+        page.locator("select[name='sgender']").selectOption("na");
+    }
+    @Test
+    void FillSignUpForm(){
+        page.navigate("https://qavbox.github.io/demo/signup/");
+    }
 
 }
